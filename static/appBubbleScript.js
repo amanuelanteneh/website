b02cb8270b50cde70cddde0efbdce7cfffec8515
amanuelanteneh <asa2rc@virginia.edu>
2020-12-25 13:55:45 -0500

    3rd

--- static/appBubbleScript.js
+++ static/appBubbleScript.js
@@ -3,11 +3,11 @@ function appBubbleFunction() {
     $('#appBubble').click(function () { //fix because someone can span tf outta this and break it
         if (clickCount % 2 == 0) {
             $('#appBubble').animate({ height: '55vh' }) //extend height on even clicks
-             clickCount++; //clickCount += 1
+             clickCount++; 
         }
         else {
             $('#appBubble').animate({ height: '5vh' }) //revert height to normal on odd num clicks
-            clickCount++; //clickCount += 1
+            clickCount++; 
         }
     });
 }
