13b583f2e5a754f776bfa562bb0d96eadeecca80
amanuelanteneh <asa2rc@virginia.edu>
2020-12-29 12:59:45 -0500

    starting simulators

---
+++ static/doublePendulumScript.js
@@ -0,0 +1,7 @@
+
+const CHART_WIDTH = 600;
+const CHART_HEIGHT = 400;
+
+const chartContainer = d3.select('svg')
+                        .attr('width',CHART_WIDTH)
+                        .attr('height', CHART_HEIGHT);
