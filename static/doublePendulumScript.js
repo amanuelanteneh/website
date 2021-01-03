474084ae041599ddcc64c8805fc8fb43bb4dafc7
amanuelanteneh <asa2rc@virginia.edu>
2021-01-03 11:51:45 -0500

    double pendulum almsot done?

--- static/doublePendulumScript.js
+++ static/doublePendulumScript.js
@@ -1,11 +1,72 @@
 var width = 710, height = 710;
-var g = 9.81; //so length is in meters 
-var l = 0.5; //length of pendulum
 var margin = {top: -20, right: 30, bottom: 40, left: 40};
-var thetaInit = 0.8;
-var xBall = l*Math.sin(thetaInit);
-var yBall = -l*Math.cos(thetaInit);
+var g = 9.81; //so length is in meters 
+var l = 0.4; //length of pendulum 1 
+var ell = 0.5; //ibid pendulum 2
+var m = 5; //mass of pendulum 1
+var M = 5; // ibid pendulum 2
+var theta = 40 * (Math.PI/180);
+var phi = 10 * (Math.PI/180);
+var thetaDot = 0;
+var phiDot = 0;
+
+var xBall1 = l*Math.sin(theta);
+var yBall1 = -l*Math.cos(theta);
+var xBall2 = ell*Math.sin(phi);
+var yBall2 = -ell*Math.cos(phi) + yBall1;
 var t = 0;
+var stepSize = .01;
+
+function uDot(theta, thetaDot, phi, phiDot) { //first ODE
+    return(thetaDot);
+}
+
+function vDot(theta, thetaDot, phi, phiDot) { //second ODE
+    val = -g*(2*m + M)*Math.sin(theta) - M*g*Math.sin(theta - (2*phi)) - 2*Math.sin(theta-phi)*M*((phiDot*phiDot*ell) + thetaDot*thetaDot*l*Math.cos(theta-phi));
+
+    val = val/( l*(2*m + M - M*Math.cos(2*theta - 2*phi)) );
+    return(val);
+}
+
+function wDot(theta, thetaDot, phi, phiDot) {
+    return(phiDot);
+}
+
+function sDot(theta, thetaDot, phi, phiDot) {
+    val = 2*Math.sin(theta-phi)*(thetaDot*thetaDot*l*(m+M) + g*(m+M)*Math.cos(theta) + phiDot*phiDot*ell*M*Math.cos(theta-phi));    
+    
+    val = val/( ell*(2*m + M - M*Math.cos(2*theta - 2*phi)) );
+    return(val);
+}
+
+function RK4() { /* 4th order Runge-Kutta solver for system of 4 equations with 4 variables */
+
+        k0 = stepSize * uDot(theta, thetaDot, phi, phiDot);
+        l0 = stepSize * vDot(theta, thetaDot, phi, phiDot);
+        q0 = stepSize * wDot(theta, thetaDot, phi, phiDot);
+        p0 = stepSize * sDot(theta, thetaDot, phi, phiDot);       
+
+        k1 = stepSize * uDot(theta+(0.5*k0), thetaDot+(0.5*l0), phi+(0.5*q0), phiDot+(0.5*p0));
+        l1 = stepSize * vDot(theta+(0.5*k0), thetaDot+(0.5*l0), phi+(0.5*q0), phiDot+(0.5*p0));
+        q1 = stepSize * wDot(theta+(0.5*k0), thetaDot+(0.5*l0), phi+(0.5*q0), phiDot+(0.5*p0));
+        p1 = stepSize * sDot(theta+(0.5*k0), thetaDot+(0.5*l0), phi+(0.5*q0), phiDot+(0.5*p0));        
+
+        k2 = stepSize * uDot(theta+(0.5*k1), thetaDot+(0.5*l1), phi+(0.5*q1), phiDot+(0.5*p1));
+        l2 = stepSize * vDot(theta+(0.5*k1), thetaDot+(0.5*l1), phi+(0.5*q1), phiDot+(0.5*p1));
+        q2 = stepSize * wDot(theta+(0.5*k1), thetaDot+(0.5*l1), phi+(0.5*q1), phiDot+(0.5*p1));
+        p2 = stepSize * sDot(theta+(0.5*k1), thetaDot+(0.5*l1), phi+(0.5*q1), phiDot+(0.5*p1));        
+
+        k3 = stepSize * uDot(theta+k2, thetaDot+l2, phi+q2, phiDot+p2);
+        l3 = stepSize * vDot(theta+k2, thetaDot+l2, phi+q2, phiDot+p2);
+        q3 = stepSize * wDot(theta+k2, thetaDot+l2, phi+q2, phiDot+p2);
+        p3 = stepSize * sDot(theta+k2, thetaDot+l2, phi+q2, phiDot+p2);        
+
+        theta = theta + ((1/6) * (k0 + 2*k1 + 2*k2 + k3));
+        thetaDot = thetaDot + ((1/6) * (l0 + 2*l1 + 2*l2 + l3));
+        phi = phi + ((1/6) * (q0 + 2*q1 + 2*q2 + q3));
+        phiDot = phiDot + ((1/6) * (p0 + 2*p1 + 2*p2 + p3));
+
+    }
 
 d3.select("canvas").attr("width", width).attr("height", height);
 
@@ -13,8 +74,7 @@ var canvas = d3.select("canvas");
 
 var context = canvas.node().getContext("2d");
 
-
-var svg = d3.select("#pendulumApp")
+var svg = d3.select("#doublePendulum")
     .attr("width", width)
     .attr("height", height)
     .append("g")
@@ -36,47 +96,76 @@ var yAxis = d3.scaleLinear()
 svg.append("g")
     .call(d3.axisLeft(yAxis));
 
-var rod = svg.append('line')  /* put before circle code so it doesn't go over the circle */
-    .attr('id', 'rod')
+
+var rod1 = svg.append('line')  /* put before circle code so it doesn't go over the circle */
+    .attr('id', 'rod1')
     .attr('x2', xAxis(0))
     .attr('y2', yAxis(0))
-    .attr('x1', xAxis(xBall))
-    .attr('y1', yAxis(yBall))
+    .attr('x1', xAxis(xBall1))
+    .attr('y1', yAxis(yBall1))
     .attr('stroke', '#FFFFFF')
     .attr('stroke-width', '2px');
 
-var ball = svg.append('circle')
-    .attr('id', 'ball')
-    .attr('cx', xAxis(xBall))
-    .attr('cy', yAxis(yBall))
+var ball1 = svg.append('circle')
+    .attr('id', 'ball1')
+    .attr('cx', xAxis(xBall1))
+    .attr('cy', yAxis(yBall1))
     .attr('r', 10)
     .style('fill', '#FFFFFF');
 
-var update = function () {
+var rod2 = svg.append('line') 
+    .attr('id', 'rod2')
+    .attr('x2', xAxis(xBall1))
+    .attr('y2', yAxis(yBall1))
+    .attr('x1', xAxis(xBall2))
+    .attr('y1', yAxis(yBall2))
+    .attr('stroke', '#FFFFFF')
+    .attr('stroke-width', '2px');
+
+var ball2 = svg.append('circle')
+    .attr('id', 'ball2')
+    .attr('cx', xAxis(xBall2))
+    .attr('cy', yAxis(yBall2))
+    .attr('r', 10)
+    .style('fill', '#FFFFFF');    
+
+function update() {
     context.beginPath();
     context.strokeStyle = '#0026FF';
+    context.moveTo(xAxis(xBall2) + margin.left, yAxis(yBall2) + margin.top);    
 
-    context.moveTo(xAxis(xBall) + margin.left, yAxis(yBall) + margin.top);    
-    t += .005;
-    var theta = thetaInit * Math.cos(Math.sqrt(g / l) * t);
-    xBall = l*Math.sin(theta);
-    yBall = -l*Math.cos(theta);
+    t += .01;
 
-    context.lineTo(xAxis(xBall) + margin.left, yAxis(yBall) + margin.top);
+    RK4();
+    xBall1 = l*Math.sin(theta);
+    yBall1 = -l*Math.cos(theta);
+    xBall2 = ell*Math.sin(phi);
+    yBall2 = -ell*Math.cos(phi) + yBall1;
+
+    context.lineTo(xAxis(xBall2) + margin.left, yAxis(yBall2) + margin.top);
     context.stroke();    
 
 
-    d3.select('#rod')
+    d3.select('#rod1')
         .attr('y2', yAxis(0))
         .attr('x2', xAxis(0))
-        .attr('y1', yAxis(yBall))
-        .attr('x1', xAxis(xBall));
+        .attr('y1', yAxis(yBall1))
+        .attr('x1', xAxis(xBall1));
 
-    d3.select('#ball')
-        .attr('cx', xAxis(xBall))
-        .attr('cy', yAxis(yBall));
-}
+    d3.select('#ball1')
+        .attr('cx', xAxis(xBall1))
+        .attr('cy', yAxis(yBall1));
+
+    d3.select('#rod2')
+        .attr('y2', yAxis(yBall1))
+        .attr('x2', xAxis(xBall1))
+        .attr('y1', yAxis(yBall2))
+        .attr('x1', xAxis(xBall2));
+
+    d3.select('#ball2')
+        .attr('cx', xAxis(xBall2))
+        .attr('cy', yAxis(yBall2));
+
+    }
 
 var runApp = setInterval(function () { update(); }, 5);
-
-
---
+++ static/simplePendulumScript.js
@@ -0,0 +1,208 @@
+var width = 710, height = 710;
+var g = 9.81; //so length is in meters 
+var l = 1.2; //length of pendulum
+var margin = {top: -20, right: 30, bottom: 40, left: 40};
+var thetaInit = 50 * (Math.PI/180);
+var theta = 50 * (Math.PI/180);
+var thetaDot = 0;
+var xBallSmallAngle = l*Math.sin(thetaInit);
+var yBallSmallAngle = -l*Math.cos(thetaInit);
+var xBallExact = l*Math.sin(thetaInit);
+var yBallExact = -l*Math.cos(thetaInit);
+var t = 0;
+var stepSize = 0.01;
+var paused = 0;
+
+$("#thetaSlider").on("change", function() {
+    paused = 1;
+    theta = $(this).val() * (Math.PI/180);
+    thetaInit = theta;
+    thetaDot = 0;
+    xBallSmallAngle = l*Math.sin(thetaInit);
+    yBallSmallAngle = -l*Math.cos(thetaInit);
+    xBallExact = l*Math.sin(thetaInit);
+    yBallExact = -l*Math.cos(thetaInit);
+    d3.select('#rodExact')
+        .attr('y2', yAxis(0))
+        .attr('x2', xAxis(0))
+        .attr('y1', yAxis(yBallExact))
+        .attr('x1', xAxis(xBallExact));
+
+    d3.select('#ballExact')
+        .attr('cx', xAxis(xBallExact))
+        .attr('cy', yAxis(yBallExact));
+
+    d3.select('#rodSmallAngle')
+        .attr('y2', yAxis(0))
+        .attr('x2', xAxis(0))
+        .attr('y1', yAxis(yBallSmallAngle))
+        .attr('x1', xAxis(xBallSmallAngle));
+
+    d3.select('#ballSmallAngle')
+        .attr('cx', xAxis(xBallSmallAngle))
+        .attr('cy', yAxis(yBallSmallAngle));
+  
+    t = 0;
+    paused = 0;
+
+});
+
+$("#lengthSlider").on("input", function() {
+    l = $(this).val();
+    thetaInit = theta;
+    thetaDot = 0;
+    xBallSmallAngle = l*Math.sin(thetaInit);
+    yBallSmallAngle = -l*Math.cos(thetaInit);
+    xBallExact = l*Math.sin(thetaInit);
+    yBallExact = -l*Math.cos(thetaInit);
+    d3.select('#rodExact')
+        .attr('y2', yAxis(0))
+        .attr('x2', xAxis(0))
+        .attr('y1', yAxis(yBallExact))
+        .attr('x1', xAxis(xBallExact));
+
+    d3.select('#ballExact')
+        .attr('cx', xAxis(xBallExact))
+        .attr('cy', yAxis(yBallExact));
+
+    d3.select('#rodSmallAngle')
+        .attr('y2', yAxis(0))
+        .attr('x2', xAxis(0))
+        .attr('y1', yAxis(yBallSmallAngle))
+        .attr('x1', xAxis(xBallSmallAngle));
+
+    d3.select('#ballSmallAngle')
+        .attr('cx', xAxis(xBallSmallAngle))
+        .attr('cy', yAxis(yBallSmallAngle));
+  
+    $("#lengthInfo").html("Length: " + $(this).val());
+});
+
+function uDot(t, theta, thetaDot) { //first ODE
+    return(thetaDot);
+}
+
+function vDot(t, theta, thetaDot) { //second ODE
+    return( (-g/l) * Math.sin(theta) );
+}
+
+function RK4() {
+        k0 = stepSize * uDot(t, theta, thetaDot);
+        l0 = stepSize * vDot(t, theta, thetaDot);
+        k1 = stepSize * uDot(t+(0.5*stepSize), theta+(0.5*k0), thetaDot+(0.5*l0) );
+        l1 = stepSize * vDot(t+(0.5*stepSize), theta+(0.5*k0), thetaDot+(0.5*l0) );
+        k2 = stepSize * uDot(t+(0.5*stepSize), theta+(0.5*k1), thetaDot+(0.5*l1) );
+        l2 = stepSize * vDot(t+(0.5*stepSize), theta+(0.5*k1), thetaDot+(0.5*l1) );
+        k3 = stepSize * uDot(t + stepSize, theta+k2, thetaDot+l2);
+        l3 = stepSize * vDot(t + stepSize, theta+k2, thetaDot+l2);
+        theta = theta + (1/6) * (k0 + 2*k1 + 2*k2 + k3);
+        thetaDot = thetaDot + (1/6) * (l0 + 2*l1 + 2*l2 + l3);
+    }
+
+d3.select("canvas").attr("width", width).attr("height", height);
+
+var canvas = d3.select("canvas");
+
+var context = canvas.node().getContext("2d");
+
+var svg = d3.select("#pendulumSmallAngle")
+    .attr("width", width)
+    .attr("height", height)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+var xAxis = d3.scaleLinear()
+    .domain([-1.5, 1.5])
+    .range([0, width]);
+
+svg.append("g")
+    .attr("transform", "translate(0," + height + ")")
+    .call(d3.axisBottom(xAxis));
+
+var yAxis = d3.scaleLinear()
+    .domain([-1.5, 1.5])
+    .range([height, 0]);
+
+svg.append("g")
+    .call(d3.axisLeft(yAxis));
+
+/*Create legend*/
+svg.append("circle").attr("cx",xAxis(-1.2)).attr("cy",yAxis(1.2)).attr("r", 6).style("fill", "#FFFFFF")
+svg.append("circle").attr("cx",xAxis(-1.2)).attr("cy",yAxis(1)).attr("r", 6).style("fill", "#000000")
+svg.append("text").attr("x", xAxis(-1.15)).attr("y", yAxis(1.2)).text("Small Angle Approximation").style("font-size", "19px").style("fill", "white").attr("alignment-baseline","middle")
+svg.append("text").attr("x", xAxis(-1.15)).attr("y", yAxis(1)).text("Exact Solution").style("font-size", "19px").style("fill", "white").attr("alignment-baseline","middle")
+
+
+var rodSmallAngle = svg.append('line')  /* put before circle code so it doesn't go over the circle */
+    .attr('id', 'rodSmallAngle')
+    .attr('x2', xAxis(0))
+    .attr('y2', yAxis(0))
+    .attr('x1', xAxis(xBallSmallAngle))
+    .attr('y1', yAxis(yBallSmallAngle))
+    .attr('stroke', '#FFFFFF')
+    .attr('stroke-width', '2px');
+
+var ballSmallAngle = svg.append('circle')
+    .attr('id', 'ballSmallAngle')
+    .attr('cx', xAxis(xBallSmallAngle))
+    .attr('cy', yAxis(yBallSmallAngle))
+    .attr('r', 10)
+    .style('fill', '#FFFFFF');
+
+var rodExact = svg.append('line')  
+    .attr('id', 'rodExact')
+    .attr('x2', xAxis(0))
+    .attr('y2', yAxis(0))
+    .attr('x1', xAxis(xBallExact))
+    .attr('y1', yAxis(yBallExact))
+    .attr('stroke', '#000000')
+    .attr('stroke-width', '2px');
+
+var ballExact = svg.append('circle')
+    .attr('id', 'ballExact')
+    .attr('cx', xAxis(xBallExact))
+    .attr('cy', yAxis(yBallExact))
+    .attr('r', 10)
+    .style('fill', '#000000');    
+
+
+
+var update = function () {
+   
+    t += .01;
+    let thetaSmallAngle = thetaInit * Math.cos(Math.sqrt(g / l) * t);
+    RK4();
+    xBallSmallAngle = l*Math.sin(thetaSmallAngle);
+    yBallSmallAngle = -l*Math.cos(thetaSmallAngle);
+    xBallExact = l*Math.sin(theta);
+    yBallExact = -l*Math.cos(theta);
+
+    d3.select('#rodExact')
+        .attr('y2', yAxis(0))
+        .attr('x2', xAxis(0))
+        .attr('y1', yAxis(yBallExact))
+        .attr('x1', xAxis(xBallExact));
+
+    d3.select('#ballExact')
+        .attr('cx', xAxis(xBallExact))
+        .attr('cy', yAxis(yBallExact));
+
+    d3.select('#rodSmallAngle')
+        .attr('y2', yAxis(0))
+        .attr('x2', xAxis(0))
+        .attr('y1', yAxis(yBallSmallAngle))
+        .attr('x1', xAxis(xBallSmallAngle));
+
+    d3.select('#ballSmallAngle')
+        .attr('cx', xAxis(xBallSmallAngle))
+        .attr('cy', yAxis(yBallSmallAngle));
+
+
+    }
+        
+
+var runApp = setInterval(function () { 
+    if (!paused) {
+    update(); } 
+                }, 2);
