df102b8fa8526caf7f67757fa6f7bdf5488877fb
amanuelanteneh <asa2rc@virginia.edu>
2021-01-08 11:57:22 -0500

    working on sphere

--- static/js/riemannSphereScript.js
+++ static/js/riemannSphereScript.js
@@ -1,90 +1,255 @@
-var origin = [480, 250], j = 16, points = [], alpha = 0, beta = 0, startAngle = Math.PI/4;
+var xSphere = [];
+var ySphere = [];
+var zSphere = [];
+var xFunc = [];
+var yFunc = [];
+var zFunc = [];
+var xProj = [];
+var yProj = [];
+var zProj = [];
+var dataFunc;
+var dataProj;
+var m = 1;
+var b = 0;
+var phiArr = [];
+var thetaArr = [];
 
-var mx, my, mouseX, mouseY; //mouse drag variables
-
-/* Begin mouse drag functions */
-function dragStart(){
-    mx = d3.event.x;
-    my = d3.event.y;
-}
-
-function dragged(){
-    mouseX = mouseX || 0;
-    mouseY = mouseY || 0;
-    beta   = (d3.event.x - mx + mouseX) * Math.PI / 230 ;
-    alpha  = (d3.event.y - my + mouseY) * Math.PI / 230  * (-1);
-    processData(surface.rotateY(beta + startAngle).rotateX(alpha - startAngle)(points), 0);
-}
-
-function dragEnd(){
-    mouseX = d3.event.x - mx + mouseX;
-    mouseY = d3.event.y - my + mouseY;
-}
-/* end mouse drag functions */
-
-
-
-var svg = d3.select('#riemannSphere').call(d3.drag().on('drag', dragged).on('start', dragStart).on('end', dragEnd)).append('g');
-
-var surface = d3._3d()
-.scale(10)
-.x(function(d){ return d.x; })
-.y(function(d){ return d.y; })
-.z(function(d){ return d.z; })
-.origin(origin)
-.rotateY(startAngle)
-.rotateX(-startAngle)
-.shape('SURFACE', j*2);
-
-function processData(data, tt){
-
-    var planes = svg.selectAll('path').data(data, function(d){ return d.plane; });
-
-    planes
-        .enter()
-        .append('path')
-        .attr('class', '_3d')
-        .attr('fill', "white")
-        .attr('opacity', 0)
-        .attr('stroke-opacity', 0)
-        .merge(planes)
-        .attr('stroke', 'black')
-        .transition().duration(tt)
-        .attr('opacity', 0.1)
-        //.attr('fill', colorize)
-        .attr('d', surface.draw);
-
-    planes.exit().remove();
-
-    d3.selectAll('._3d').sort(surface.sort);
-
-}
-
-function init(eq){
-    points = [];
-
-    for(var z = -j; z < j; z++){
-        for(var x = -j; x < j; x++){
-            points.push({x: x, y: eq(x, z), z: z});
-        }
+$("#slopeSlider").on("change", function() { 
+    m = Number($(this).val());
+    $("#slopeInfo").html("Slope = " + m);
+    Plotly.deleteTraces('riemannSphere', 2);
+    Plotly.deleteTraces('riemannSphere', 2); //after you delete a trace all of them shift up a spot
+    zFunc = [];
+    xFunc = [];
+    yFunc = [];
+    zProj = [];
+    xProj = [];
+    yProj = [];
+    for (x=-1; x<1; x+=0.01) {
+        zFunc.push(0);
+        yFunc.push(theFunction(1,x*m + b));
+        xFunc.push(x);
     }
+    getProjection(1, -10, 10);
+    dataFunc = [{
+        name: 'Function',
+        opacity: 1,
+        type: 'scatter3d',
+        mode: 'lines',
+        x: xFunc,
+        y: yFunc,
+        z: zFunc,
+        line: {
+            width: 5,
+            color: 'red',
+        }
+    }];
+    dataProj = [{
+        name: 'Projection',
+        opacity: 1,
+        type: 'scatter3d',
+        mode: 'lines',
+        x: xProj,
+        y: yProj,
+        z: zProj,
+        line: {
+            width: 5,
+            color: 'blue',
+        }
+    }];
+    Plotly.addTraces('riemannSphere', dataFunc);
+    Plotly.addTraces('riemannSphere', dataProj);
 
-    var yMin = d3.min(points, function(d){ return d.y; });
-    var yMax = d3.max(points, function(d){ return d.y; });
+}
+);
+$("#interceptSlider").on("change", function() { 
+    b = Number($(this).val());
+    $("#interceptInfo").html("Y-intercept = " + b);
+    Plotly.deleteTraces('riemannSphere', 2);
+    Plotly.deleteTraces('riemannSphere', 2); //after you delete a trace all of them shift up a spot
+    zFunc = [];
+    xFunc = [];
+    yFunc = [];
+    zProj = [];
+    xProj = [];
+    yProj = [];
+    for (x=-1; x<1; x+=0.01) {
+        zFunc.push(0);
+        yFunc.push(theFunction(1,m*x+b));
+        xFunc.push(x);
+    }
+    getProjection(1, -10, 10);
+    dataFunc = [{
+        name: 'Function',
+        opacity: 1,
+        type: 'scatter3d',
+        mode: 'lines',
+        x: xFunc,
+        y: yFunc,
+        z: zFunc,
+        line: {
+            width: 5,
+            color: 'red',
+        }
+    }];
+    dataProj = [{
+        name: 'Projection',
+        opacity: 1,
+        type: 'scatter3d',
+        mode: 'lines',
+        x: xProj,
+        y: yProj,
+        z: zProj,
+        line: {
+            width: 5,
+            color: 'blue',
+        }
+    }];
+    Plotly.addTraces('riemannSphere', dataFunc);
+    Plotly.addTraces('riemannSphere', dataProj);
+}
+);
 
- //   color.domain([yMin, yMax]);
-    processData(surface(points), 1000);
+
+function theFunction(choice, x) {
+    switch(choice) {
+        case 1: return(m*x + b);
+        case 2: return(Math.tan(x));
+        default: return(NaN);
+    }
 }
 
-function change(){
-    var rn1 = Math.floor(d3.randomUniform(1, 12)());
-    rn1 = 1;
-    var eqa = function(x, z) {
-        return Math.sqrt( 400 - (z*z) - (x*x) )*rn1;
-    };
-    init(eqa);
+
+function makeInterval(startValue, stopValue, numPoints) {
+    var arr = [];
+    var step = (stopValue - startValue) / (numPoints - 1);
+    for (var i = 0; i < numPoints; i++) {
+      arr.push(startValue + (step * i));
+    }
+    return arr;
 }
 
-d3.selectAll('button').on('click', change);
+function getProjection(choice, a, b) {
+    xProj = [];
+    yProj = [];
+    zProj = [];
+    let stepSize = (b-a)/150;
+    for (x=a; x<b; x+=stepSize) {
+        let y = theFunction(choice, x);
+        xProj.push(x*( 2 / ((x*x + y*y) + 1) ));
+        yProj.push(y*( 2 / ((x*x + y*y) + 1) ));
+        zProj.push( ((x*x + y*y) - 1) / ((x*x + y*y) + 1) ); 
+    }
+}
 
-change();
+phiArr = makeInterval(0, Math.PI/2, 35); //make array of values from [0, Pi/2] with 25 values
+thetaArr = makeInterval(0, 2*Math.PI, 35); //ibid
+
+for (i=0; i<thetaArr.length; i++){
+    for (j=0; j<phiArr.length; j++){
+        xSphere.push(Math.cos(thetaArr[i]) * Math.sin(phiArr[j]));
+        ySphere.push(Math.sin(thetaArr[i]) * Math.sin(phiArr[j]));   
+        zSphere.push(Math.cos(phiArr[j]));
+    }
+}
+
+const dataItem = {
+    name: 'Sphere',
+    opacity: 0.3,
+    color: 'rgb(211,211,211)',
+    type: 'mesh3d',
+    x: xSphere,
+    y: ySphere,
+    z: zSphere,
+};
+
+var dataSphere = [
+    dataItem,
+    {...dataItem, z: zSphere.map(v => -v)} //to plot top and bottom semi-sphere
+
+];
+
+
+var layout = {
+    title: 'The Riemann Sphere',
+    autosize: true,
+    plot_bgcolor:"#FFFFFF",
+    paper_bgcolor:"#333333",
+    scene: {
+        yaxis: {
+            "tickcolor": "white",
+            "backgroundcolor": "white",
+            "gridcolor": "white",
+            "zerolinecolor": "white",
+        },
+        xaxis: {
+            "tickcolor": "white",
+            "gridcolor": "white",
+            "backgroundcolor": "white",
+            "zerolinecolor": "white",
+        },
+        zaxis: {
+            "tickcolor": "white",
+            "gridcolor": "white",
+            "backgroundcolor": "white",
+            "zerolinecolor": "white",
+        },
+    },
+    font: {
+        family: 'Courier New, monospace',
+        size: 12,
+        color: '#FFFFFF'
+     },
+    showlegend: true,
+    width: 600,
+    height: 600,
+    margin: {
+        l: 65,
+        r: 50,
+        b: 65,
+        t: 90,
+    }
+};
+Plotly.newPlot('riemannSphere', dataSphere, layout);
+
+
+for (x=-1; x<1; x+=0.01) {
+    zFunc.push(0);
+    yFunc.push(theFunction(1,x));
+    xFunc.push(x);
+}
+
+
+var dataFunc = [{
+    name: 'Function',
+    opacity: 1,
+    type: 'scatter3d',
+    mode: 'lines',
+    x: xFunc,
+    y: yFunc,
+    z: zFunc,
+    line: {
+        width: 5,
+        color: 'red',
+    }
+}];
+
+Plotly.addTraces('riemannSphere', dataFunc);
+
+getProjection(1,-10, 10);
+
+var dataProj = [{
+    name: 'Projection',
+    opacity: 1,
+    type: 'scatter3d',
+    mode: 'lines',
+    x: xProj,
+    y: yProj,
+    z: zProj,
+    line: {
+        width: 5,
+        color: 'blue',
+    }
+}];
+
+Plotly.addTraces('riemannSphere', dataProj);
