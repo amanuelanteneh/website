d2742c9cd0a07459a85adcfd09214de3d34ac490
amanuelanteneh <asa2rc@virginia.edu>
2021-01-09 11:15:56 -0500

    fixing navbar for mobile

--- static/js/navBar.js
+++ static/js/navBar.js
@@ -1,5 +1,12 @@
-const hamMenu = document.getElementsByClassName("ham-menu")[0]
-const navButtons = document.getElementsByClassName("navButtons")[0] 
+const hamMenu = document.getElementsByClassName("ham-menu")[0];
+const navButtons = document.getElementsByClassName("navButtons")[0];
 hamMenu.addEventListener('click', () => { 
-    navButtons.classList.toggle('active')
-})
+    navButtons.classList.toggle('active');
+});
+
+$("li").on("click", function() {
+    console.log("html =" + $(this).attr("id") );
+    if ($(this).attr("id") != "personal") {
+        navButtons.classList.toggle('active');
+    }
+});
