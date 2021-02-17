ce0001b02ff78668001e22ab03b7bbf9d619aa00
amanuelanteneh <asa2rc@virginia.edu>
2021-02-17 09:54:24 -0500

    fixed little padding issue

--- static/js/navBar.js
+++ static/js/navBar.js
@@ -1,12 +1,18 @@
 const hamMenu = document.getElementsByClassName("ham-menu")[0];
 const navButtons = document.getElementsByClassName("navButtons")[0];
+const bar1 = document.getElementById("bar1");
+const bar2 = document.getElementById("bar2");
+const bar3 = document.getElementById("bar3");
 hamMenu.addEventListener('click', () => { 
+   /* bar1.style.visibility = "hidden" ;
+    bar2.style.transform = "rotate(" + -70 + "deg)";
+    bar3.style.transform = "rotate(" + 70 + "deg)";    */
     navButtons.classList.toggle('active');
 });
 
 //to make menu collapse once an option is picked after its been expanded on mobile
 $("li").on("click", function() {
-    console.log("html =" + $(this).attr("id") );
+
     if ($(this).attr("id") != "personal" && $(this).attr("id") != "pdfs" && $(this).attr("id") != "cv/github" && $(this).attr("class") == "navBar-li" ) {
         navButtons.classList.toggle('active');
     }
