c75ae7f2fd50d7968253dcdf6e5a2c38e9c0bd4d
amanuelanteneh <asa2rc@virginia.edu>
2021-02-20 16:56:00 -0500

    little update

--- static/js/quoteChange.js
+++ static/js/quoteChange.js
@@ -1,15 +1,16 @@
-let quote1 = 'The philosophers have only <u>interpreted</u> the world, in various ways. The point, however, is to change it.<br> - <i>Theses on Feuerbach</i>'
-let quote2 = 'We are more free than ever before to look around in all directions; nowhere do we perceive any limits. We have the advantage of feeling an immense space around us- <u>but also an immense void.</u><br> - <i>Introduction to Modernity</i>' 
-let quote3 = 'We learn history not in order to know how to behave or how to succeed, <u>but to know who we are.</u><br> - <i>Modernity on Endless Trial</i>'
-let quote4 = 'As the archaeology of our thought easily shows, man is an invention of recent date. And one perhaps <u>nearing its end</u>.<br> - <i>The Order of Things</i>'
-let quotes = [quote1, quote2, quote3, quote4];
+let quote1 = 'Hello my name is Amanuel Anteneh, welcome to my website!<br>• Computer Science •<br>• Physics •<br>• Mathematics •'
+let quote2 = 'The philosophers have only <u>interpreted</u> the world, in various ways. The point, however, is to change it.<br> - <i>Theses on Feuerbach</i>'
+let quote3 = 'We are more free than ever before to look around in all directions; nowhere do we perceive any limits. We have the advantage of feeling an immense space around us- <u>but also an immense void.</u><br> - <i>Introduction to Modernity</i>' 
+let quote4 = 'We learn history not in order to know how to behave or how to succeed, <u>but to know who we are.</u><br> - <i>Modernity on Endless Trial</i>'
+let quote5 = 'As the archaeology of our thought easily shows, man is an invention of recent date. And one perhaps <u>nearing its end</u>.<br> - <i>The Order of Things</i>'
+let quotes = [quote1, quote2, quote3, quote4, quote5];
 let index = 1;
 
 $(document).ready(function () {
     $("#floatingQuote").click(function () {
         $("#floatingQuote").fadeOut(function () {
             $("#floatingQuote").html(quotes[index]).fadeIn();             
-            if (index == 3) {
+            if (index == 4) {
              index = 0;
             }
             else {
