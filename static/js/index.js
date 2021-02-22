86cc2b9de37d4feaa7c875408040e6a5b1e6b03d
amanuelanteneh <asa2rc@virginia.edu>
2021-02-22 15:57:12 -0500

    fix dropdown items

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,26 @@
-//script is for main page (index) 
 const hamMenu = document.getElementsByClassName("ham-menu")[0];
 var navButtons = document.getElementsByClassName("navButtons")[0];
-const bar1 = document.getElementById("bar1");
-const bar2 = document.getElementById("bar2");
-const bar3 = document.getElementById("bar3");
+
+var interests = document.getElementById("interests");
+var interestsMenu = document.getElementById("interests-menu");
+var interestsArrow = document.getElementById("interests-arrow");
+
+var pdfs = document.getElementById("pdfs");
+var pdfsMenu = document.getElementById("pdfs-menu");
+var pdfsArrow = document.getElementById("pdfs-arrow");
+
+var cvGithub = document.getElementById("cv-github");
+var cvGithubMenu = document.getElementById("cv-github-menu");
+var cvGithubArrow = document.getElementById("cv-github-arrow");
+
+var bar1 = document.getElementById("bar1");
+var bar2 = document.getElementById("bar2");
+var bar3 = document.getElementById("bar3");
 bar1.style.transition = "transform 0.2s linear";
 bar3.style.transition = "transform 0.2s linear";  
-navButtons.style.transition = "transform 0.2s linear"; 
-var hamMenuOpen = 0;
+navButtons.style.transition = "transform 0.2s linear";
+
+var hamMenuOpen = 0; 
 hamMenu.addEventListener('click', () => { 
     if (!hamMenuOpen) {    //hamburger menu squish animation
         bar1.style.transform = "translateY(" + "9px" + ")";
@@ -17,15 +30,68 @@ hamMenu.addEventListener('click', () => {
     else {
         bar1.style.transform = "translateY(" + "-0.5px" + ")";
         bar3.style.transform = "translateY(" + "0.5px" + ")"; 
-        navButtons.classList.toggle('active');        
+        interestsMenu.style.display = "none";
+        pdfsMenu.style.display = "none";        
+        cvGithubMenu.style.display = "none"; 
+        navButtons.classList.toggle('active');
     }
     hamMenuOpen = !hamMenuOpen;
 });
 
+interests.addEventListener('click', () => { 
+    if ($(window).width() >= 940) {
+        return;
+    }
+    if (interestsMenu.style.display == "none") {
+        interestsMenu.style.display = "block";
+        cvGithubMenu.style.display = "none";
+        pdfsMenu.style.display = "none";
+    }
+    else {
+        interestsMenu.style.display = "none";
+        cvGithubMenu.style.display = "none";
+        pdfsMenu.style.display = "none";
+    }
+});
+
+pdfs.addEventListener('click', () => {
+    if ($(window).width() >= 940) {
+        return;
+    }
+    if (pdfsMenu.style.display == "none") {
+        pdfsMenu.style.display = "block";
+        cvGithubMenu.style.display = "none";
+        interestsMenu.style.display = "none";
+    }
+    else {
+        interestsMenu.style.display = "none";
+        cvGithubMenu.style.display = "none";
+        pdfsMenu.style.display = "none";
+    }
+});
+
+cvGithub.addEventListener('click', () => { 
+    if ($(window).width() >= 940) {
+        return;
+    }
+    if (cvGithubMenu.style.display == "none") {
+        pdfsMenu.style.display = "none";
+        cvGithubMenu.style.display = "block";
+        interestsMenu.style.display = "none";
+    }
+    else {
+        interestsMenu.style.display = "none";
+        cvGithubMenu.style.display = "none";
+        pdfsMenu.style.display = "none";
+    }
+});
+
 //to make menu collapse once an option is picked after its been expanded on mobile
 $("li").on("click", function() {
-
-    if ($(this).attr("id") != "interests" && $(this).attr("id") != "pdfs" && $(this).attr("id") != "cv/github" && $(this).attr("class") == "navBar-li" ) {
+    if ($(window).width() >= 940) {
+        return; // only run func if window size is for mobile, else hover stops working when clicking navbar items bc of lines 110-112
+    }
+    if ($(this).attr("id") != "interests" && $(this).attr("id") != "pdfs" && $(this).attr("id") != "cv-github" && $(this).attr("class") == "navBar-li" ) {
         if (!hamMenuOpen) {    //hamburger menu squish animation
         bar1.style.transform = "translateY(" + "9px" + ")";
         bar3.style.transform = "translateY(" + "-9px" + ")"; 
@@ -34,42 +100,42 @@ $("li").on("click", function() {
         else {
         bar1.style.transform = "translateY(" + "-0.5px" + ")";
         bar3.style.transform = "translateY(" + "0.5px" + ")"; 
-        navButtons.classList.toggle('active');        
-            }
+        navButtons.classList.toggle('active');   
+        interestsMenu.style.display = "none";
+        pdfsMenu.style.display = "none";        
+        cvGithubMenu.style.display = "none"; 
+        }
     hamMenuOpen = !hamMenuOpen;
     
     }
 });
 
 $(window).on('load', function() { //check width when page loads to see if u should use bootstrap dropdown class or nah
-    if($(window).width() < 940) {
-        document.getElementById("pdfs menu").classList.add('dropdown');
-        document.getElementById("pdfs menu").classList.remove('menu');
-        document.getElementById("interests menu").classList.add('dropdown');
-        document.getElementById("interests menu").classList.remove('menu');
-        document.getElementById("cv-github menu").classList.add('dropdown');
-        document.getElementById("cv-github menu").classList.remove('menu');        
+    if($(window).width() <= 940) {
+        document.getElementById("pdfs-menu").classList.add('dropdown');
+        document.getElementById("pdfs-menu").classList.remove('menu');
+        document.getElementById("interests-menu").classList.add('dropdown');
+        document.getElementById("interests-menu").classList.remove('menu');
+        document.getElementById("cv-github-menu").classList.add('dropdown');
+        document.getElementById("cv-github-menu").classList.remove('menu');        
     }
 })
 
 $(window).on('resize', function() { //same but for if window is resized
-    if($(window).width() < 940) {
-        document.getElementById("pdfs menu").classList.add('dropdown');
-        document.getElementById("pdfs menu").classList.remove('menu');
-        document.getElementById("interests menu").classList.add('dropdown');
-        document.getElementById("interests menu").classList.remove('menu');
-        document.getElementById("cv-github menu").classList.add('dropdown');
-        document.getElementById("cv-github menu").classList.remove('menu');        
+    if($(window).width() <= 940) {
+        document.getElementById("pdfs-menu").classList.add('dropdown');
+        document.getElementById("pdfs-menu").classList.remove('menu');
+        document.getElementById("interests-menu").classList.add('dropdown');
+        document.getElementById("interests-menu").classList.remove('menu');
+        document.getElementById("cv-github-menu").classList.add('dropdown');
+        document.getElementById("cv-github-menu").classList.remove('menu');        
     }
-})
-
-$(window).on('resize', function() { //ibid but switch to no bootstrap class for big screens
-    if($(window).width() > 940) {
-        document.getElementById("pdfs menu").classList.remove('dropdown');
-        document.getElementById("pdfs menu").classList.add('menu');
-        document.getElementById("interests menu").classList.remove('dropdown');
-        document.getElementById("interests menu").classList.add('menu');
-        document.getElementById("cv-github menu").classList.remove('dropdown');
-        document.getElementById("cv-github menu").classList.add('menu');        
+    else {
+        document.getElementById("pdfs-menu").classList.remove('dropdown');
+        document.getElementById("pdfs-menu").classList.add('menu');
+        document.getElementById("interests-menu").classList.remove('dropdown');
+        document.getElementById("interests-menu").classList.add('menu');
+        document.getElementById("cv-github-menu").classList.remove('dropdown');
+        document.getElementById("cv-github-menu").classList.add('menu');        
     }
-})
+});
